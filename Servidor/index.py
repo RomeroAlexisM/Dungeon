c075e0a66ac27d23a0b92e7488a45f3b9317d3c2
piquiferrato <juanferrato1994@gmail.com>
2018-04-30 17:19:51 -0300

    Cambios generales

--- Servidor/index.py
+++ Servidor/index.py
@@ -1,1 +1,34 @@
-print("holaaaaa")
+from Servidor.clases import Enemigo
+
+
+class Rata(Enemigo):
+    def __init__(self):
+        super().__init__(name="Rata",
+                         ps=10,
+                         fuerza=2,
+                         agilidad=8,
+                         vitalidad=6,
+                         energia=10,
+                         exp=5)
+
+
+class Troll(Enemigo):
+    def __init__(self):
+        super().__init__(name="Orco",
+                         ps=25,
+                         fuerza=10,
+                         agilidad=4,
+                         vitalidad=30,
+                         energia=30,
+                         exp=10)
+
+
+class Dragon(Enemigo):
+    def __init__(self):
+        super().__init__(name="Dragon",
+                         ps=100,
+                         fuerza=40,
+                         agilidad=30,
+                         vitalidad=80,
+                         energia=60,
+                         exp=40)
