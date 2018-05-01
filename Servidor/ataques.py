580ed2462738a96d80867012365d2f2c1d1366b5
piquiferrato <juanferrato1994@gmail.com>
2018-05-01 02:39:45 -0300

    ataques enemigos añadidos, cambios generales

--- Servidor/ataques.py
+++ Servidor/ataques.py
@@ -10,6 +10,7 @@ class Golpear(Ataque):
                          costoMana=0,
                          danioBasico=3)
 
+
 class Ensartada(Ataque):
     def __init__(self):
         super().__init__(nombre="Ensartada",
@@ -32,4 +33,50 @@ class LightingStorm(Ataque):
                          costoMana=8,
                          danioBasico=8)
 
+# Ataques de la rata
 
+
+class Mordisco(Ataque):
+    def __init__(self):
+        super().__init__(nombre="Mordisco",
+                         costoMana=0,
+                         danioBasico=4)
+
+
+class Escupitajo(Ataque):
+    def __init__(self):
+        super().__init__(nombre="Escupitajo",
+                         costoMana=0,
+                         danioBasico=6)
+
+# Ataques del orco
+
+
+class GolpeGarrote(Ataque):
+    def __init__(self):
+        super().__init__(nombre="Golpe de Garrote",
+                         costoMana=0,
+                         danioBasico=8)
+
+
+class RompeCraneos(Ataque):
+    def __init__(self):
+        super().__init__(nombre="Rompecráneos",
+                         costoMana=0,
+                         danioBasico=15)
+
+# Ataques del Dragón
+
+
+class Coletazo(Ataque):
+    def __init__(self):
+        super().__init__(nombre="Coletazo",
+                         costoMana=0,
+                         danioBasico=19)
+
+
+class Llamarada(Ataque):
+    def __init__(self):
+        super().__init__(nombre="Llamarada",
+                         costoMana=0,
+                         danioBasico=23)
--- Servidor/enemigos.py
+++ Servidor/enemigos.py
@@ -1,3 +1,4 @@
+from Servidor.ataques import *
 from Servidor.clases import Enemigo
 
 
@@ -9,26 +10,29 @@ class Rata(Enemigo):
                          agilidad=8,
                          vitalidad=6,
                          energia=10,
-                         exp=5)
+                         exp=5,
+                         ataques=(Mordisco, Escupitajo))
 
 
 class Troll(Enemigo):
     def __init__(self):
         super().__init__(name="Orco",
-                         ps=10,
-                         fuerza=2,
-                         agilidad=8,
-                         vitalidad=6,
+                         ps=25,
+                         fuerza=20,
+                         agilidad=14,
+                         vitalidad=30,
                          energia=10,
-                         exp=5)
+                         exp=15,
+                         ataques=(GolpeGarrote, RompeCraneos))
 
 
 class Dragon(Enemigo):
     def __init__(self):
         super().__init__(name="Dragon",
-                         ps=10,
-                         fuerza=2,
-                         agilidad=8,
-                         vitalidad=6,
-                         energia=10,
-                         exp=5)
+                         ps=45,
+                         fuerza=70,
+                         agilidad=30,
+                         vitalidad=90,
+                         energia=50,
+                         exp=5,
+                         ataques=(Coletazo, Llamarada))
