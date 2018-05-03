7036081f2a33c8c995635749024f3d775659a2d2
piquiferrato <juanferrato1994@gmail.com>
2018-05-03 17:09:18 -0300

    Arreglada importante de diseño

--- Servidor/clases.py
+++ Servidor/clases.py
@@ -1,25 +1,63 @@
+MODIFICADOR_DANIO_BASICO = .15
+MODIFICADOR_DANIO_ESPECIAL = .30
+PUNTOS_NUEVO_NIVEL = 5
+VALOR_REGENERACION_VIDA = .2
+VALOR_REGENERACION_MANA = .24
+CERO = 0
 
+MENSAJE_FALTA_MANA = 'No tienes suficiente mana'
 
 
 class Entidad:
-    def __init__(self, ps, mana, fuerza, agilidad, vitalidad, energia, exp, ataques):
-        self.ps = ps
-        self.mana = mana
-        self.fuerza = fuerza
-        self.agilidad = agilidad
-        self.vitalidad = vitalidad
-        self.energia = energia
-        self.exp = exp
+    def __init__(self, id, ps, mana, fuerza, agilidad, vitalidad, energia, exp, ataques):
+        self.id = str(id)
+        self.ps = int(ps)
+        self.mana = int(mana)
+        self.fuerza = int(fuerza)
+        self.agilidad = int(agilidad)
+        self.vitalidad = int(vitalidad)
+        self.energia = int(energia)
+        self.exp = int(exp)
         self.ataques = ataques
+        self.atributobase = CERO
+
+    def set_atributobase(self, atributobase):
+        self.atributobase = atributobase
+
+    def atacar(self, enemigo, ataque, atributobase):
+
+        if ataque.especial:
+
+            if self.suficiente_mana(ataque.costoMana):
+                enemigo.ps -= ataque.calcular_danio(MODIFICADOR_DANIO_ESPECIAL, atributobase)
+                self.perder_mana(ataque.costoMana)
+
+            else:
+                print(MENSAJE_FALTA_MANA)
+
+        else:
+            enemigo.ps -= ataque.calcular_danio(MODIFICADOR_DANIO_BASICO, atributobase)
+
+    def renegerar_vida(self):
+        self.ps += int(self.vitalidad * VALOR_REGENERACION_VIDA)
+
+    def regenerar_mana(self):
+        self.mana += int(self.energia * VALOR_REGENERACION_MANA)
+
+    def perder_mana(self, mana):
+        self.mana -= mana
+
+    def nuevo_nivel(self):
+        self.fuerza += PUNTOS_NUEVO_NIVEL
+        self.agilidad += PUNTOS_NUEVO_NIVEL
+        self.vitalidad += PUNTOS_NUEVO_NIVEL
+        self.energia += PUNTOS_NUEVO_NIVEL
+
+    def suficiente_mana(self, costomana):
+        return self.mana >= costomana
 
     def vive(self):
         return self.ps > 0
-
-    def atacar(self):
-        pass
-
-
-
 
 class Item:
     def __init__(self, nombre, descripcion, equipado):
@@ -42,7 +80,6 @@ class Ataque:
 
     def calcular_danio(self, modificador, atributobase):
         return modificador * atributobase
-
 
 
 
--- Servidor/enemigos.py
+++ Servidor/enemigos.py
@@ -1,37 +1,31 @@
 from Servidor.ataques import *
 from Servidor.clases import Entidad
 
-class Rata(Entidad):
-    def __init__(self):
-        super().__init__(name="Rata",
-                         ps=10,
-                         fuerza=2,
-                         agilidad=8,
-                         vitalidad=6,
-                         energia=10,
-                         exp=5,
-                         ataques=(Mordisco, Escupitajo))
+rata = Entidad(id='Rata',
+               ps=10,
+               fuerza=2,
+               agilidad=8,
+               vitalidad=6,
+               energia=10,
+               exp=5,
+               ataques=(mordisco, escupitajo))
 
 
-class Troll(Entidad):
-    def __init__(self):
-        super().__init__(name="Orco",
-                         ps=25,
-                         fuerza=20,
-                         agilidad=14,
-                         vitalidad=30,
-                         energia=10,
-                         exp=15,
-                         ataques=(GolpeGarrote, RompeCraneos))
+troll = Entidad(id='Orco',
+                ps=25,
+                fuerza=20,
+                agilidad=14,
+                vitalidad=30,
+                energia=10,
+                exp=15,
+                ataques=(golpeGarrote, rompeCraneos))
 
 
-class Dragon(Entidad):
-    def __init__(self):
-        super().__init__(name="Dragon",
-                         ps=45,
-                         fuerza=70,
-                         agilidad=30,
-                         vitalidad=90,
-                         energia=50,
-                         exp=5,
-                         ataques=(Coletazo, Llamarada))
+dragon = Entidad(id='Dragon',
+                 ps=45,
+                 fuerza=70,
+                 agilidad=30,
+                 vitalidad=90,
+                 energia=50,
+                 exp=5,
+                 ataques=(coletazo, llamarada))
