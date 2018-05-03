5675d8106ec9d62df204053ed87a2a7ad31a9837
Alexis Romero <romeroalexism@gmail.com>
2018-05-03 20:22:27 -0300

    "Se mueve por todos los mapas"

--- Cliente/js/juego.js
+++ Cliente/js/juego.js
@@ -3,12 +3,30 @@ var Juego = {
 
   ancho: 900,
   alto: 500,
+  mapaActual: 0,
 
-  jugador: new Jugador( 'images/jugador1.png',131,270,15,30),
+  jugador: new Jugador( 'images/jugador1.png',71,190,15,30),
+
+  puerta:[
+    new Puerta(890,895,360,420),
+    new Puerta(110,240,470,490),
+    new Puerta(0,-20,10,90),
+    new Puerta(885,900,170,480),
+    new Puerta(885,900,170,480),
+    new Puerta(345,735,0,-20),
+    new Puerta(885,900,200,270)
+  ],
 
   mapa:[
-    new Mapa('images/mapa1.png',0,0,0,891,360,420),
-    new Mapa('images/mapa2.png',891,360,420,0,0,0)
+    new Mapa('images/mapa1.png', 0, 1),
+    new Mapa('images/mapa2.png', 1, 2),
+    new Mapa('images/mapa4.png', 2, 3),
+    new Mapa('images/mapa3.png', 3, 4),
+    new Mapa('images/mapa4.png', 4, 5),
+    new Mapa('images/mapa5.png', 5, 6),
+    new Mapa('images/mapa6.png', 6, 7),
+    new Mapa('images/mapa7.png', 7, 8)
+
   ]
 }
 
@@ -18,6 +36,9 @@ Juego.iniciarRecursos = function() {
     'images/mapa2.png',
     'images/mapa3.png',
     'images/mapa4.png',
+    'images/mapa5.png',
+    'images/mapa6.png',
+    'images/mapa7.png',
     'images/jugador1.png'
   ]);
   Resources.onReady(this.comenzar.bind(Juego));
@@ -41,7 +62,7 @@ Juego.buclePrincipal = function() {
 };
 
 Juego.update = function() {
-  // console.log("x: "+this.jugador.x+" y:"+this.jugador.y)
+  console.log("x: "+this.jugador.x+" y:"+this.jugador.y)
   // this.calcularAtaques();
   // this.moverEnemigos();
 }
@@ -59,27 +80,87 @@ Juego.capturarMovimiento = function(tecla) {
 
 Juego.dibujar = function() {
   for (var i = 0; i < this.mapa.length; i++) {
-    if (this.estaEnLaPuerta(this.mapa[i])) {
+    if (this.pasoPorLaPuerta(this.mapa[i].numeroPuerta)) {
       // Borrar el fotograma actual
         Dibujante.borrarAreaDeJuego();
         //Se pinta la imagen de fondo segun el estado del juego
         this.dibujarFondo(this.mapa[i+1].sprite);
-        console.log(this.mapa[i+1].sprite);
-        // this.jugador.x = 10;
+
+        Dibujante.dibujarEntidad(this.jugador);
+        this.mapaActual++;
+this.posicionarJugador(this.mapa[i+1].numeroMapa, this.jugador.x, this.jugador.y);
+        // console.log("se");
+
     }else {
+      if (i == this.mapaActual) {
         Dibujante.borrarAreaDeJuego();
-        this.dibujarFondo(this.mapa[0].sprite);
-        console.log("se");
+        this.dibujarFondo(this.mapa[i].sprite);
+
+        Dibujante.dibujarEntidad(this.jugador);
+        // this.posicionarJugador(this.mapa[i].numeroMapa);
+        // console.log("no");
+      }
     }
   };
 
   // this.dibujarFondo();
-  Dibujante.dibujarEntidad(this.jugador);
+
 };
 
-Juego.estaEnLaPuerta = function(mapa) {
-  return ((this.jugador.y) < mapa.puertaSalidaYB && (this.jugador.y) >
-          mapa.puertaSalidaYA && (this.jugador.x) == mapa.puertaSalidaX);
+Juego.pasoPorLaPuerta = function(numeroPuerta) {
+  for (var i = 0; i < this.puerta.length; i++) {
+    if (i == numeroPuerta) {
+      if (numeroPuerta == 2) {
+        return((this.jugador.y) > this.puerta[i].posicionYA && (this.jugador.y) <
+                this.puerta[i].posicionYB && (this.jugador.x) <
+                this.puerta[i].posicionXA && (this.jugador.x) >
+                this.puerta[i].posicionXB);
+      }
+      if (numeroPuerta == 5) {
+        return((this.jugador.y) < this.puerta[i].posicionYA && (this.jugador.y) >
+                this.puerta[i].posicionYB && (this.jugador.x) >
+                this.puerta[i].posicionXA && (this.jugador.x) <
+                this.puerta[i].posicionXB);
+      }
+      return((this.jugador.y) > this.puerta[i].posicionYA && (this.jugador.y) <
+              this.puerta[i].posicionYB && (this.jugador.x) >
+              this.puerta[i].posicionXA && (this.jugador.x) <
+              this.puerta[i].posicionXB);
+    }
+  }
+};
+
+Juego.posicionarJugador = function(numeroMapa, posicionJugadorX, posicionJugadorY) {
+  console.log(numeroMapa);
+switch (numeroMapa) {
+  case 1:
+    this.jugador.x = 71;
+    this.jugador.y = 190;
+    break;
+  case 2:
+    this.jugador.x = 10;
+    break;
+  case 3:
+    this.jugador.y = 10;
+    break;
+  case 4:
+    this.jugador.x = 871;
+    break;
+  case 5:
+    this.jugador.x = 5;
+    break;
+  case 6:
+    this.jugador.x = 5;
+    break;
+  case 7:
+    this.jugador.y = 490;
+    break;
+  case 8:
+    this.jugador.y = 5;
+    break;
+  default:
+
+}
 };
 
 Juego.dibujarFondo = function(url) {
--- Cliente/js/mapa.js
+++ Cliente/js/mapa.js
@@ -1,9 +1,5 @@
-var Mapa = function(sprite, puertaEntradaX, puertaEntradaYA, puertaEntradaYB, puertaSalidaX, puertaSalidaYA, puertaSalidaYB){
+var Mapa = function(sprite, numeroPuerta, numeroMapa){
   this.sprite = sprite;
-  this.puertaEntradaX = puertaEntradaX;
-  this.puertaEntradaYA = puertaEntradaYA;
-  this.puertaEntradaYB = puertaEntradaYB;
-  this.puertaSalidaX = puertaSalidaX;
-  this.puertaSalidaYA = puertaSalidaYA;
-  this.puertaSalidaYB = puertaSalidaYB;
+  this.numeroPuerta = numeroPuerta;
+  this.numeroMapa = numeroMapa;
 }
