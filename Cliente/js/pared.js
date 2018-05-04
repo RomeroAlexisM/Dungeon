6bf76415f6061811454286594663a26941105b01
Alexis Romero <romeroalexism@gmail.com>
2018-05-03 21:26:16 -0300

    "agregado mapa completo"

---
+++ Cliente/js/pared.js
@@ -0,0 +1,6 @@
+var Puerta = function(posicionXA, posicionXB, posicionYA, posicionYB){
+  this.posicionXA = posicionXA;
+  this.posicionXB = posicionXB;
+  this.posicionYA = posicionYA;
+  this.posicionYB = posicionYB;
+}
