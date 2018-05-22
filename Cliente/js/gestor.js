d6587c2199cbe8d630abd7198f3fa559ad2f9cc5
Alexis Romero <romeroalexism@gmail.com>
2018-05-21 23:40:54 -0300

    reacomodo

--- Cliente/js/gestor.js
+++ Cliente/js/gestor.js
@@ -1,7 +1,16 @@
-function peticionGet() {
+ var servidor = 'http://127.0.0.1:5000';
+function players() {
 
- axios.get('/telefonos').then(function(response) {
- console.log(response.data);
+ axios.get(servidor + '/images/character-selection/').then(function(response) {
+   // for (var i = 0; i < response.data.length; i++) {
+        console.log(response.data);
+   // }
+
+   // $('#guerrero').append("Clase: "+response.data.playables_list[0].nombre+'<br>');
+   //  $('#guerrero').append("Energia: "+response.data.playables_list[0].energia);
+   // $('#mago').append("Clase: "+response.data.playables_list[1].nombre);
+   //  $('#mago').append("     Energia: "+response.data.playables_list[1].energia);
+
  }).catch(function(error) {
  console.log(error);
  });
