5eb7b9ecacdb312aa198d524ad3d2a0a770aea73
Alexis Romero <romeroalexism@gmail.com>
2018-05-23 16:32:44 -0300

    agregado de imagenes de cada personaje

--- Cliente/js/gestor.js
+++ Cliente/js/gestor.js
@@ -1,19 +1,28 @@
  var servidor = 'http://127.0.0.1:5000';
+ //muestra todos los personajes para seleccionar
 function players() {
 
  axios.get(servidor + '/images/character-selection/').then(function(response) {
-   var cantidad = response.data.length;
+   var personajes = response.data.character_list
+   var cantidad = response.data.character_list.length;
    for (var i = 0; i <cantidad; i++) {
-     console.log("es");
-     console.log(i);
-        console.log(response.data.character_list[i].nombre);
+     //se clona un elemento que funciona como ejemplo de como se van a mostrar los personajes por pantalla
+     var personaje = $(".ejemplo").clone();
+     //se cargan los datos de las películas
+     personaje.find(".imagen").attr("src", personajes[i].url);
+     personaje.attr("id", personajes[i].id);
+     //se agrega el personaje que al contenedor de personaje
+     personaje.appendTo($(".contenedor"));
+     //este personaje no va a ser mas de la clase ejemplo
+     personaje.removeClass("ejemplo");
+     //se muestra el personaje que se creo
+     personaje.show();
+
+     personaje.click(function() {
+      var id = $(this).attr('id');
+      console.log(id);
+     });
    }
-
-   // $('#guerrero').append("Clase: "+response.data.playables_list[0].nombre+'<br>');
-   //  $('#guerrero').append("Energia: "+response.data.playables_list[0].energia);
-   // $('#mago').append("Clase: "+response.data.playables_list[1].nombre);
-   //  $('#mago').append("     Energia: "+response.data.playables_list[1].energia);
-
  }).catch(function(error) {
  console.log(error);
  });
