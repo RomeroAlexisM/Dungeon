bfd1b5951afca09f115d7ce856ce1893852e2ec3
piquiferrato <juanferrato1994@gmail.com>
2018-05-23 13:11:33 -0300

    agregado manejo de cuentas y sesiones

--- Servidor/api/app.py
+++ Servidor/api/app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-from flask_cors import CORS
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-CORS(app)
 app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
@@ -12,6 +11,13 @@ db = SQLAlchemy(app)
 from core.src.Controllers.EntidadController import *
 from core.src.Controllers.EnemyController import *
 from core.src.Controllers.ImageController import *
+from core.src.Controllers.UserController import *
+
 
 if __name__ == '__main__':
+    # user_manager = UserManager()
+    # user_manager.init_app(app)
+    # login_manager = LoginManager()
+    # login_manager.init_app(app)
+    # login_manager.login_view = 'login'
     app.run()
--- Servidor/api/config.py
+++ Servidor/api/config.py
@@ -1,2 +1,11 @@
+
+from flask_login import LoginManager
+
+from api.app import app
+
 DEBUG = True
+SECRET_KEY = 'thisisasecret'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///db.db'
+CSRF_ENABLED = True
+USER_ENABLE_EMAIL = False
+
